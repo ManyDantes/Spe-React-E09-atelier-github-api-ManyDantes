daf025d78fc4c12631c218c60e6f0b60c48f39ea
Eden-Cadagiani-Oclock <eden@oclock.io>
2022-05-03 16:02:26 +0200

    update render to react 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // == Import : npm
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 // == Import : local
 // Composants
@@ -9,7 +9,9 @@ import App from 'src/components/App';
 // 1. Élément React racine (celui qui contient l'ensemble de l'app)
 //    => crée une structure d'objets imbriqués (DOM virtuel)
 const rootReactElement = <App />;
+
 // 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
-const target = document.getElementById('root');
+const root = createRoot(document.getElementById('root'));
+
 // 3. Déclenchement du rendu de React (virtuel) => DOM (page web)
-render(rootReactElement, target);
+root.render(rootReactElement);
